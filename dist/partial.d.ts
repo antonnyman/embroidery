382e2397be5bedfc8723077b4d1c08dc7b2d4337
Anton Nyman <anton@antonnyman.se>
2020-07-20 08:37:27 +0200

    Remove dist catalog

--- dist/partial.d.ts
+++
@@ -1,6 +0,0 @@
-export default class Partial {
-    private dataAttr;
-    private element;
-    constructor(element: Element);
-    create(): Promise<void>;
-}
--- dist/utils.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function domReady(): Promise<unknown>;
-export declare const defaultEventNames: {
-    a: string;
-    button: string;
-    form: string;
-    input: string;
-    select: string;
-    textarea: string;
-};
